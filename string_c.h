0d3a2a936537b6cffa37dab5d1f14014728369f3
Sebastian Rautenbach <55651172+SebastianRautenbach@users.noreply.github.com>
2023-05-06 22:55:54 +0200

    Update string_c.h

--- string_c.h
+++ string_c.h
@@ -11,19 +11,19 @@
 #pragma once
 #include <memory>
 #include <stdlib.h>
+#include <ostream>
 
 class _string {
 public:
 	char* m_string;
-	unsigned int size = 0;
 	_string(const char* string)
 	{
-		if(string == nullptr){
+		if (string == nullptr) {
 			m_string = new char[1];
 			m_string[0] = '\0';
 		}
 		else {
-			size = strlen(string);
+			unsigned int size = strlen(string);
 			m_string = new char[size + 1];
 			strncpy_s(m_string, size + 1, string, size);
 			m_string[size] = '\0';
@@ -36,7 +36,7 @@ public:
 		m_string[str_size] = '\0';
 	}
 	_string()
-		:m_string{nullptr}
+		:m_string{ nullptr }
 	{
 		m_string = new char[1];
 		m_string[0] = '\0';
@@ -98,7 +98,7 @@ public:
 			}
 		}
 		return -1;
-	}	
+	}
 	int _find(char _start_f, _string& word, char _end_f, short _start) {
 		int index = 0;
 		for (unsigned int i = _start; i < _length(); i++)
@@ -128,7 +128,9 @@ public:
 	}
 	_string& operator=(char c_char) {
 		if (m_string[0] == c_char)
-			{return *this;}
+		{
+			return *this;
+		}
 		delete[] m_string;
 		m_string = new char[2];
 		m_string[0] = c_char;
@@ -137,10 +139,19 @@ public:
 	}
 	_string& operator=(_string& other_string) {
 		if (this == &other_string)
-			{return *this;}
+		{
+			return *this;
+		}
 		delete[] m_string;
 		m_string = new char[strlen(other_string.m_string) + 1];
 		strncpy_s(m_string, strlen(other_string.m_string) + 1, other_string.m_string, strlen(other_string.m_string));
+		return *this;
+	}
+	_string& operator=(const char* c_char_ptr)
+	{
+		delete[] m_string;
+		m_string = new char[strlen(c_char_ptr) + 1];
+		strncpy_s(m_string, strlen(c_char_ptr) + 1, c_char_ptr, strlen(c_char_ptr));
 		return *this;
 	}
 	void operator+=(_string& other_string) {
@@ -150,7 +161,7 @@ public:
 		strncat_s(temp_strn, st_size, other_string.m_string, other_string._length());
 		delete[] m_string;
 		m_string = new char[st_size + 1];
-		strncpy_s(m_string, st_size + 1, temp_strn , st_size);
+		strncpy_s(m_string, st_size + 1, temp_strn, st_size);
 		m_string[st_size] = '\0';
 		delete[] temp_strn;
 	}
@@ -168,11 +179,16 @@ public:
 	}
 	void operator+(_string& other_string) {
 		int total_str_size = other_string._length() + _length();
-		char* new_gen_char = new char[total_str_size +1];
+		char* new_gen_char = new char[total_str_size + 1];
 		strncpy_s(new_gen_char, total_str_size + 1, m_string, _length());
 		strncat_s(new_gen_char, total_str_size + 1, other_string.m_string, other_string._length());
 		new_gen_char[total_str_size] = '\0';
 		strncpy_s(m_string, total_str_size + 1, new_gen_char, total_str_size);
 		delete[] new_gen_char;
 	}
-}; 
+	friend std::ostream& operator<<(std::ostream& os, _string& other_string)
+	{
+		os << other_string.m_string;
+		return os;
+	}
+};
